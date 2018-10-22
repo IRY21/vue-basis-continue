547955901ba14efb9295791a748822d1ef4a405e
IRY21 <rilok21work@gmail.com>
2018-10-22 16:04:25 +0500

    start

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
   <div>
-      <h2>{{ title }}</h2>
+      <h2>Form inputs</h2>
 
-      <input type="text" v-model="searchName">
-
-      <ul>
-        <li v-for="name of filteredNames">{{ name }}</li>
-      </ul>
-
-      <hr>
-
-      <appList></appList>
+      <input type="text" >
   </div>
 </template>
 
 <script>
-import ListMixin from './listMixin'
 
 export default {
   data() {
     return {
-      title: "Hello I am Vue!"
+      name: ""
     };
-  },
-
-  mixins: [ListMixin],
-
-  filters: {
-    lowercase(val) {
-      return val.toLowerCase();
-    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import List from './List.vue'
-
-Vue.filter('uppercase', (val) => val.toUpperCase());
-
-Vue.component('appList', List);
-
-Vue.mixin({
-  beforeCreate() {
-    console.log('before create');
-  } 
-});
 
 new Vue({ 
   el: '#app',
