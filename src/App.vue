82e94456c540e96e2d4b8258ff12d1ad288865d2
IRY21 <rilok21work@gmail.com>
2018-10-23 17:26:41 +0500

    dinamic routs

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,20 @@
           <!-- <li class="nav-item active">
             <router-link class="nav-link" :to="'/cars'">Cars</router-link>
           </li> -->
+
+
           <router-link tag="li" class="nav-item" exact to="/" active-class="active">
             <a class="nav-link">Home</a>
           </router-link>
           <router-link tag="li" class="nav-item" to="/cars" active-class="active">
             <a class="nav-link">Cars</a>
+          </router-link>
+
+          <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
+            <a class="nav-link">Car 3</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
+            <a class="nav-link">Car 4</a>
           </router-link>
         </ul>
       </div>
