6bd1f87adda31b5039f659b4a738cca469e7397f
IRY21 <rilok21work@gmail.com>
2018-10-26 12:59:27 +0500

    start

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,6 @@
 <template>
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <!-- <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li> -->
-          <!-- <li class="nav-item active">
-            <router-link class="nav-link" :to="'/cars'">Cars</router-link>
-          </li> -->
-
-
-          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
-            <a class="nav-link">Home</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" to="/cars" active-class="active">
-            <a class="nav-link">Cars</a>
-          </router-link>
-
-          <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
-            <a class="nav-link">Car 3</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
-            <a class="nav-link">Car 4</a>
-          </router-link>
-        </ul>
-      </div>
-    </nav>
-    <router-view></router-view>
+    
   </div>
 </template>
 
