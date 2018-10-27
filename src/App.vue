a94ee6677d361c5647aa33cafc4f12009acd0085
IRY21 <rilok21work@gmail.com>
2018-10-27 10:03:34 +0500

    getters

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="container text-center pt-5">
-    <app-counter></app-counter>
-    <app-actions></app-actions>
+    <appCounter></appCounter>
+    <appSecondCounter></appSecondCounter>
+    <hr>
+    <appActions></appActions>
   </div>
 </template>
 
 <script>
   import Counter from './Counter';
+  import SecondCounter from './SecondCounter';
   import Actions from './Actions';
 
   export default {
     components: {
       appCounter: Counter,
+      appSecondCounter: SecondCounter,
       appActions: Actions
     }
   };
